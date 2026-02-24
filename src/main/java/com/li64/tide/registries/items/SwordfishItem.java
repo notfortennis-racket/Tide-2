76896ccd835ac5d6056d1b5cb8b521a6acfe413b
Lightning-64 <averyg8833@gmail.com>
2026-02-24 15:46:12 -0500

    Sword fishes are no longer repairable

--- src/main/java/com/li64/tide/registries/items/SwordfishItem.java
+++ src/main/java/com/li64/tide/registries/items/SwordfishItem.java
@@ -1,5 +1,6 @@
 package com.li64.tide.registries.items;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 
@@ -7,5 +8,10 @@ public class SwordfishItem extends SwordItem {
     public SwordfishItem(Tiers tier, int damageMod, float speedMod, Properties properties) {
         /*? if >=1.21 {*/super(tier, properties.attributes(SwordItem.createAttributes(tier, damageMod, speedMod)));
         /*?} else*//*super(tier, damageMod, speedMod, properties);*/
+    }
+
+    @Override
+    public boolean isValidRepairItem(ItemStack stack, ItemStack repairCandidate) {
+        return false;
     }
 }
